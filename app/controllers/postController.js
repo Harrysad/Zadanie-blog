5819e741dfdbc20c0aa12751a1b7042fd365e31f
Sebastian Dębowski <s_debowski@wp.pl>
2024-11-03 13:12:30 +0100

    Problem z wyświetleniem add w nawigacji

---
+++ app/controllers/postController.js
@@ -0,0 +1,56 @@
+const Post = require('../models/PostModel');
+
+module.exports = {
+    index: (req, res) => {
+        Post.find({})
+            .lean()
+            .then((posts) => {
+                res.render("blogViews/blog", { posts: posts });
+            })
+            .catch((err) => {
+                res.send(err)
+            })
+    },
+    post: (req, res) => {
+        Post.findById(req.params.id)
+            .then((post) => {
+                res.render("blogViews/singlePost", post);
+            })
+            .catch((err) => {
+                res.send(err);
+            });
+    },
+    create: (req, res) => {
+        const newPost = new Post({ ...req.body, author: "Misio" });
+        newPost.save();
+
+        res.redirect("/blog");
+    },
+    update: (req, res) => {
+        Post.findByIdAndUpdate(req.params.id, req.body)
+            .then((post) => {
+                res.redirect("/blog/" + post._id);
+            })
+            .catch((err) => {
+                res.send(err);
+            });
+    },
+    delete: (req, res) => {
+        Post.findByIdAndDelete(req.params.id)
+            .then(() => {
+                res.redirect("/blog");
+            })
+            .catch((err) => {
+                res.send(err);
+            });
+    },
+    editForm: (req, res) => {
+        Post.findById(req.params.id)
+            .then((post) => {
+                res.render("blogViews/editPost", post);
+            })
+            .catch((err) => {
+                res.send(err);
+            });
+    },
+};
---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const hbs = require("express-handlebars");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/express-blog");
+
+
+const Post = require('./app/models/PostModel')
+
+const blogRouter = require('./app/router/blogRouter');
+
+app.use("/files", express.static("public"));
+
+app.engine("hbs", hbs.engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+app.use(express.urlencoded({extended: true}));
+
+app.get("/mongoose/:id", function (req, res) {
+  Post.findById(req.params.id).then((post)=>{
+    res.render("home", {
+      title: post.title,
+      content: post.content,
+      displayTitle: true,
+      names: ["Adam", "Ola", "Kasia", "Tomek"],
+    });
+  }).catch((err)=>{
+    res.send(err)
+  })
+  
+});
+
+app.get("/", function (_req, res) {
+  res.render("home", {
+    title: "My app title",
+    content: "Lorem ipsum",
+    displayTitle: true,
+    names: ["Adam", "Ola", "Kasia", "Tomek"],
+  });
+});
+/* Routes */
+app.use("/blog", blogRouter);
+
+
+app.listen(8080, function () {
+  console.log("Serwer Node.js działa");
+});
